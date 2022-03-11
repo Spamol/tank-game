264f4c127d210bbbea3e7f1addbfce8381c2ee65
Maxim Spesivykh <spamol@ya.ru>
2022-03-11 20:22:04 +0300

    writing tests

--- main.js
+++ main.js
@@ -1,4 +1,18 @@
-function sum(a, b) {
-  return a + b;
+class Main {
+  solve(a, b, c) {
+    if(Math.floor(a) === 0) return false;
+    if(typeof a !== 'number' || typeof b !== 'number' || typeof c !== 'number') return false;
+    const res = [];
+    const D = b * b - 4 * a * c;
+    if(D < 0) return [];
+    if(Math.floor(D) == 0)
+        res.push((-b + Math.sqrt(D)) / (2 * a));
+    else if(D > 0){
+        res.push((-b + Math.sqrt(D)) / (2 * a));
+        res.push((-b - Math.sqrt(D)) / (2 * a));
+    }
+    return res;
+  }
 }
-module.exports = sum;
+
+module.exports = Main;
--- main.test.js
+++ main.test.js
@@ -1,5 +1,23 @@
-const sum = require('./main');
+const Main = require('./main');
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+const main = new Main();
+
+test('x^2+1 = 0', () => {
+  expect(main.solve(1, 0, 1)).toEqual([ ]);
+});
+
+test('x^2-1 = 0', () => {
+  expect(main.solve(1, 0, -1)).toEqual([ 1, -1 ]);
+});
+
+test('discriminant > 0 && discriminant < epsilon', () => {
+  expect(main.solve(2, 1.65, 0.3)).toEqual([ -0.2705272913549932 ]);
+});
+
+test('a > 0 && a < 1', () => {
+  expect(main.solve(0.3, 1, 1)).toEqual(false);
+});
+
+test('a, b, c not numbers', () => {
+  expect(main.solve('1', false, null)).toEqual(false);
 });
